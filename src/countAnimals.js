39561d06eb298e983e8a9191b15945f3384b2756
Luiz Arlochi <luiz.arlochi@gmail.com>
2023-03-09 20:51:19 -0300

    requisito 5 concluido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (!animal) {
+    return data.species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+
+  const { species, sex } = animal;
+  const speciesData = data.species.find((getSpecies) => getSpecies.name === species);
+  if (!sex) {
+    return speciesData.residents.length;
+  }
+  return speciesData.residents.filter((getResident) => getResident.sex === sex).length;
 };
 
 module.exports = countAnimals;
