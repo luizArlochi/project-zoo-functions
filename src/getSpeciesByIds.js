f9afa54e7889a05ddc1d1bc20e8d3442cb5e9791
Luiz Arlochi <luiz.arlochi@gmail.com>
2023-03-09 17:46:33 -0300

    requisito 1 concluido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  if (ids.length === 0) {
+    return [];
+  }
+  return data.species.filter((animal) => ids.includes(animal.id));
 };
 
 module.exports = getSpeciesByIds;
