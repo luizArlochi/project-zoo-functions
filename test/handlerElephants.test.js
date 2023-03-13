8f7e6d72ce5f92f993a52c7a1cc744a627d3dc30
Luiz Arlochi <luiz.arlochi@gmail.com>
2023-03-13 18:14:50 -0300

    requisitos 6, 7 e 15 concluidos

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,39 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  describe('when the parameter is undefined', () => {
+    it('should return undefined', () => {
+      expect(handlerElephants(undefined)).toBeUndefined();
+    });
+  });
+
+  describe('when the parameter is not a string', () => {
+    it('should return an error message', () => {
+      expect(handlerElephants(123)).toBe('Parâmetro inválido, é necessário uma string');
+    });
+  });
+
+  describe('when the parameter is "count"', () => {
+    it('should return the count of elephant residents', () => {
+      expect(handlerElephants('count')).toBe(4);
+    });
+  });
+
+  describe('when the parameter is "names"', () => {
+    it('should return an array of elephant names', () => {
+      expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+    });
+  });
+
+  describe('when the parameter is "averageAge"', () => {
+    it('should return the average age of elephant residents', () => {
+      expect(handlerElephants('averageAge')).toBe(10.5);
+    });
+  });
+
+  describe('when the parameter is not valid', () => {
+    it('should return null', () => {
+      expect(handlerElephants('invalidParam')).toBeNull();
+    });
+  });
+});
