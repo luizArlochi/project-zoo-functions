121ced34f1615e7e28d352f074d08f473be30294
Luiz Arlochi <luiz.arlochi@gmail.com>
2023-03-13 22:06:01 -0300

    requisito 8 +/- concluido, passando nos testes mas não no lint

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  if (!Array.isArray(entrants) || entrants.length === 0) {
+    return { child: 0, adult: 0, senior: 0 };
+  }
+
+  return entrants.reduce((acc, { age }) => {
+    if (age < 18) {
+      acc.child += 1;
+    } else if (age < 50) {
+      acc.adult += 1;
+    } else {
+      acc.senior += 1;
+    }
+    return acc;
+  }, { child: 0, adult: 0, senior: 0 });
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  const count = countEntrants(entrants);
+
+  const totalPrice = Object.entries(count)
+    .reduce((acc, [age, countAge]) => acc + data.prices[age] * countAge, 0);
+
+  return Number(totalPrice.toFixed(2));
 };
 
 module.exports = { calculateEntry, countEntrants };
