175bfdab2511c5a3c0430abbf31b277865ed43ab
Luiz Arlochi <luiz.arlochi@gmail.com>
2023-03-13 22:34:20 -0300

    ajuste no calculateEntry pro lint parar de reclamar

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,18 +1,22 @@
 const data = require('../data/zoo_data');
 
+const categorizeAge = (age) => {
+  if (age < 18) {
+    return 'child';
+  } if (age < 50) {
+    return 'adult';
+  }
+  return 'senior';
+};
+
 const countEntrants = (entrants) => {
   if (!Array.isArray(entrants) || entrants.length === 0) {
     return { child: 0, adult: 0, senior: 0 };
   }
 
   return entrants.reduce((acc, { age }) => {
-    if (age < 18) {
-      acc.child += 1;
-    } else if (age < 50) {
-      acc.adult += 1;
-    } else {
-      acc.senior += 1;
-    }
+    const category = categorizeAge(age);
+    acc[category] += 1;
     return acc;
   }, { child: 0, adult: 0, senior: 0 });
 };
